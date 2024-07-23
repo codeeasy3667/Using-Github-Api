040aa97557ff9e347b6b2e1819728b9e02027fef
roshan-77 <roshan.khadka.u@gmail.com>
2024-07-23 22:00:59 +1000

    updated name

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var gitprojects = document.querySelector(".container")
 
-fetch("https://api.github.com/users/codeeasy3667/repos").then(
+fetch("https://api.github.com/users/{username}/repos").then(
     response => {
         if(response.ok){
             return response.json()
@@ -14,13 +14,13 @@ fetch("https://api.github.com/users/codeeasy3667/repos").then(
         if(result[i].stargazers_count !== 0){
             var proj = `<div class="projects-container">
                             <div class="header">
-                                <img src=${`https://raw.githubusercontent.com/codeeasy3667/${result[i].name}/${result[i].default_branch}/Thumbnail/thumbnail.png`} alt="thumbnail" class="src">
+                                <img src=${`https://raw.githubusercontent.com/{username}/${result[i].name}/${result[i].default_branch}/Thumbnail/thumbnail.png`} alt="thumbnail" class="src">
                             </div>
                             <div class="title"><strong>${result[i].name}</strong></div>
                             <div class="description">${result[i].description}</div>
                             <div class="links">
-                                <a href="https://github.com/codeeasy3667/${result[i].name}" target="_blank">Github Repo</a>
-                                <a href="https://codeeasy3667.github.io/${result[i].name}" target="_blank">Website</a>
+                                <a href="https://github.com/{username}/${result[i].name}" target="_blank">Github Repo</a>
+                                <a href="https://{username}.github.io/${result[i].name}" target="_blank">Website</a>
                             </div>
                         </div>`
             gitprojects.insertAdjacentHTML('beforeend', proj);
